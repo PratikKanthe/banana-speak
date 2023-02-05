79153def3dc659a7347fed502bd5d079031e261c
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2023-02-05 10:52:26 +0530

    added input

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var btnTranslate = document.querySelector('.btn-translate');
+var textInput = document.querySelector('.text-input');
 
 document.addEventListener('click', function clickEventHandler(){
-    console.log("clicked");
+    console.log('clicked');
+    console.log('input: ',textInput.value);
 });
+
+
